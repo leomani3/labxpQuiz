38ba64c0617d55de8070038645d32e129217e86a
Leomani <leoveyssiere.pro@hotmail.fr>
2020-01-10 16:13:10 +0100

    blblbl4

---
+++ server.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var session = require('express-session');
+var app = express();
+
+var session = require("express-session")({
+    secret: "my-secret",
+    resave: true,
+    saveUninitialized: true
+});
+var sharedsession = require("express-socket.io-session");
+
+var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream('Questions.txt')
+});
+
+app.use(session);
+
+
+
+// la base
+var io = require('socket.io')({
+	transports: ['websocket'],
+});
+
+io.use(sharedsession(session, {
+    autoSave:true
+}));
+
+io.attach(5000);
+
+
+// include our files
+var tools = require('./Game');
+tools.Main(io);
+
+
+global.playerNumber = 0;
+global.numberQuestion = 0;
+global.nbPlayerAnswer = 0;
+
+global.scorePlayer = new Map();
+global.namePlayerMap = new Map();
+global.nbJoueurGetScore=0;
+
+console.log("sa tourne au calme");
+
+i =0;
+global.questions = [];
+
+lineReader.on("line", function (line) {	
+	
+	question = {};
+
+	answer = {};
+	questionAndResponses = line.split("//")[0];
+	indexResponse = line.split("//")[1];
+	
+	questionTitle = questionAndResponses.split("#&")[0];
+	responses = questionAndResponses.split("#&")[1];
+	responsesString = responses.split("/");
+	for(j = 0 ; j < responsesString.length ; j++){
+		response = responses.split("/")[j];
+		answer[j] =  response;
+	}
+	
+	question = {"title" : questionTitle,"goodAnswer" : indexResponse ,answer };
+	
+	questions.push(question);
+	i++;
+});
+
+
+
